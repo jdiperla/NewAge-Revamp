24dd212fc48dabca4f83c0406296b636f81fb497
jdiperla <jdiperla@gmail.com>
2020-02-21 12:01:36 -0500

    Create synonyms.js
    Created initial Synonym file for Looking, Examining, Speaking, Using, Operating and taking. More to come.

---
+++ includes/synonyms.js
@@ -0,0 +1,118 @@
+<script>
+
+var synonyms = {
+//Looking
+     'look': [
+      [OBJECTLOOK]
+      ],
+      'view': [
+      [OBJECTLOOK]
+      ],
+      'leer': [
+      [OBJECTLOOK]
+      ],
+      'look-see': [
+      [OBJECTLOOK]
+      ],
+ //Examining
+      'examine': [
+      [OBJECTEXAMINE]
+      ],
+      'investigate': [
+      [OBJECTEXAMINE]
+      ],
+      'survey': [
+      [OBJECTEXAMINE]
+      ],
+      'inspect': [
+      [OBJECTEXAMINE]
+      ],
+      'review': [
+      [OBJECTEXAMINE]
+      ],
+      'check': [
+      [OBJECTEXAMINE]
+      ],
+      'explore': [
+      [OBJECTEXAMINE]
+      ],
+//Taking      
+      'take': [
+      [OBJECTTAKE]
+      ],
+      'collect': [
+      [OBJECTTAKE]
+      ],
+      'pick up': [
+      [OBJECTTAKE]
+      ],
+       'take': [
+      [OBJECTTAKE]
+      ],
+      'seize': [
+      [OBJECTTAKE]
+      ],
+      'acquire': [
+      [OBJECTTAKE]
+      ],
+      'attain': [
+      [OBJECTTAKE]
+      ],
+      'obtain': [
+      [OBJECTTAKE]
+      ],
+//Using/Interacting
+      'use': [
+      [OBJECTUSE]
+      ],
+      'interact': [
+      [OBJECTUSE]
+      ],
+      'handle': [
+      [OBJECTUSE]
+      ],
+      'utilize': [
+      [OBJECTUSE]
+      ],
+      'wield': [
+      [OBJECTUSE]
+      ],
+      'make use': [
+      [OBJECTUSE]
+      ],
+ //Operating      
+     'operate': [
+      [OBJECTOPERATE]
+      ],
+      'manage': [
+      [OBJECTOPERATE]
+      ],
+      'manipulate': [
+      [OBJECTOPERATE]
+      ],
+      'work': [
+      [OBJECTOPERATE]
+      ],
+//Speak or talk to
+      'talk': [
+      [OBJECTTALK]
+      ],
+      'speak': [
+      [OBJECTTALK]
+      ],
+      'chat': [
+      [OBJECTTALK]
+      ],
+      'converse': [
+      [OBJECTTALK]
+      ],
+      
+     
+     ]
+
+}
+
+
+
+
+</script>
