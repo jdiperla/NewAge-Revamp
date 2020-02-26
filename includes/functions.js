1f6a1b9bf2fcda8ec39ad31e9220450c3ef83c24
jdiperla <jdiperla@gmail.com>
2020-02-26 13:36:52 -0500

    Update functions.js
    Implemented cmdNoExit()

--- includes/functions.js
+++ includes/functions.js
@@ -5,4 +5,12 @@ function replaceAll(str, find, replace) {
 }   //end function ParsePlayerInput
 
 
+function cmdNoExit() {
+ //Whenever the player wants to go in a direction that does not exist at the current location, the exit message will throw out a random
+ //exit message from the global list.
+  rndExitMsgLngth = globalmsg['noexit'].length;
+  rndExitMsgRst = Math.floor(Math.random() * rndExitMsgLngth);
+  return globalmsg['noexit'][rndExitMsgRst]);
+}
+
 <script>
