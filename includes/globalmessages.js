1a565cebfa8f1bdcbe7baf139b7cd78656ae2007
jdiperla <jdiperla@gmail.com>
2020-03-02 15:38:58 -0500

    Update globalmessages.js
    Adding error messages for commands not found.

--- includes/globalmessages.js
+++ includes/globalmessages.js
@@ -3,7 +3,11 @@
 var globalmsg = {
 //For no exit found
      'noexit': [
-      ["You can't go that way."], ['There is no exit in that direction']
-      ]
+          ["You can't go that way."], ["There is no exit in that direction"]
+      ],
+     'nocmd': [
+          ["Quit spinning your wheels."], ["You can't do that."], ["Please stop trying that."], ["No, just.. NO!"]
+          ]
+}
 
 <script>
