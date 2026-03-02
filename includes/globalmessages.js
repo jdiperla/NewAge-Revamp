67e770ef491d72f86b1f09da7c8770c9b99bdfd9
jdiperla <jdiperla@gmail.com>
2026-03-02 15:01:23 -0500

    Add room-specific object command handlers in game data

--- includes/globalmessages.js
+++ includes/globalmessages.js
@@ -1,13 +1,10 @@
-<script>
 
 var globalmsg = {
 //For no exit found
      'noexit': [
-          ["You can't go that way."], ["There is no exit in that direction"]
+          ["You can't go that way."], ["There is no exit in that direction."]
       ],
      'nocmd': [
           ["Quit spinning your wheels."], ["You can't do that."], ["Please stop trying that."], ["No, just.. NO!"]
           ]
-}
-
-<script>
+};
